2205fb82a7d3b3043151c936463d5d9c5978d303
Tim Morgan <tim@timmorgan.org>
2023-05-27 19:50:32 -0500

    Fix class name, oops

--- lib/tables/user.rb
+++ lib/tables/user.rb
@@ -2,7 +2,7 @@ module Tables
   class User < ActiveRecord::Base
     validates :call_sign, presence: true
 
-    belongs_to :monitoring_net
+    belongs_to :monitoring_net, class_name: 'Net'
     has_many :favorites, dependent: :delete_all
 
     scope :is_monitoring, -> { where.not(monitoring_net_id: nil) }
