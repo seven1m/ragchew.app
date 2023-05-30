69dab091ce62e9d9cbb3aa8bbe094b7b5f8fbe82
Tim Morgan <tim@timmorgan.org>
2023-05-29 21:52:22 -0500

    Use SameSite=lax
    Maybe this will fix the logout issue.

--- app.rb
+++ app.rb
@@ -7,7 +7,7 @@ require_relative './boot'
 
 enable :sessions
 set :session_secret, ENV.fetch('SESSION_SECRET') { SecureRandom.hex(64) }
-set :sessions, same_site: :strict, expire_after: 365 * 24 * 60 * 60 # 1 year
+set :sessions, same_site: :lax, expire_after: 365 * 24 * 60 * 60 # 1 year
 disable :protection
 
 set :static_cache_control, [:public, max_age: 60]
